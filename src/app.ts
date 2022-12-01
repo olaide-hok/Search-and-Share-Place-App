77d19769b1ace572298cffb368a990eb140432aa
olaide-hok <olaidekareem43@yahoo.com>
2022-12-01 15:34:39 +0100

    Set up app.ts file.
    Add Google API KEY.
    
    Add type definition for Google geocoding response.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-// Code goes here!
+const form = document.querySelector('form')!
+const addressInput = document.getElementById('address')! as HTMLInputElement
+
+const GOOGLE_API_KEY = "AIzaSyBEP4cOYGPyGxV8onjcSdJniLb5u1YfAbY"
+
+type GoogleGeocodingResponse = {
+    results: {geometry: {location: {lat: number; lng: number} }}[];
+    status: "OK" | "ZERO_RESULTS"
+}
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault()
+    // const enteredAddress = addressInput.value
+
+}
+
+form.addEventListener('submit', searchAddressHandler)
