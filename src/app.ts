9567dce32d813dc3016b9e4076efa3b0cdbee566
olaide-hok <olaidekareem43@yahoo.com>
2022-12-01 16:55:07 +0100

    Complete code.
    Complete searchAddressHandler function.

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,39 @@
-const form = document.querySelector('form')!
-const addressInput = document.getElementById('address')! as HTMLInputElement
+import axios from "axios";
 
-const GOOGLE_API_KEY = "AIzaSyBEP4cOYGPyGxV8onjcSdJniLb5u1YfAbY"
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "AIzaSyBEP4cOYGPyGxV8onjcSdJniLb5u1YfAbY";
 
 type GoogleGeocodingResponse = {
-    results: {geometry: {location: {lat: number; lng: number} }}[];
-    status: "OK" | "ZERO_RESULTS"
-}
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
 
 function searchAddressHandler(event: Event) {
-    event.preventDefault()
-    // const enteredAddress = addressInput.value
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
 
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    ).then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could b=not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: coordinates,
+        zoom: 16,
+      });
+
+      new google.maps.Marker({ position: coordinates, map: map });
+    }).catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
-form.addEventListener('submit', searchAddressHandler)
+form.addEventListener("submit", searchAddressHandler);
